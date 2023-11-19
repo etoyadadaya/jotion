4e82c39dac0c519128806f2b66e43be8884bd507
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-11-19 16:18:56 +0700

    Banner

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { Id } from '@/convex/_generated/dataModel';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useRouter } from 'next/navigation';
+import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
+import { MoreHorizontalIcon, TrashIcon } from 'lucide-react';
+import { Skeleton } from '@/components/ui/skeleton';
+
+interface MenuProps {
+  documentId: Id<'documents'>;
+}
+
+export const Menu = ({ documentId }: MenuProps) => {
+  const { user } = useUser();
+  const router = useRouter();
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = () => {
+    const promise = archive({
+      id: documentId,
+    });
+
+    toast.promise(promise, {
+      loading: 'Moving to trash...',
+      success: 'Note moved to trash!',
+      error: 'Failed to archive note.',
+    });
+
+    router.push('/documents');
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size='sm' variant='ghost'>
+          <MoreHorizontalIcon className='h-4 w-4' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-60'
+        align='end'
+        alignOffset={8}
+        forceMount
+      >
+        <DropdownMenuItem onClick={onArchive}>
+          <TrashIcon className='mr-2 h-4 w-4' />
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className='p-2 text-xs text-muted-foreground'>
+          Last edited by: {user?.fullName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return <Skeleton className='h-10 w-10' />;
+};
